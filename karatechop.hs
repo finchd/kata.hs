55dae5a81e97a3c3b6e5f29c31b3cd0fac1b611c
Donovan Finch <finchd@server.fake>
2014-03-24 15:21:38 -0700

    Add codekata.com Kata 02 : karatechop.hs. codekata.com/kata/kata02-karate-chop/

---
+++ karatechop.hs
@@ -0,0 +1,9 @@
+{- My solution to http://codekata.com/kata/kata02-karate-chop/
+ - Kata #02 from
+ - codekata.com
+ - Author: Donovan Finch
+ - Goal: a binary search of a sorted integer list.
+ - -}
+
+chop :: Int -> [Int] -> Int
+
